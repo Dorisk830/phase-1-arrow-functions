84e46bdd76772a82e54c3e86dafc46b6faa62d24
DorisK830 <doris.kiprop@student.moringaschool.com>
2023-10-06 23:15:59 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+function divide(divide) {
+divide =(2000 / 100);
+return divide;
+
+}
+const square = (parameter1, parameter2) => parameter1 ** parameter1;
+square(2,2);
+
+const add = (a, b) => a + b;
+add (3,4)
